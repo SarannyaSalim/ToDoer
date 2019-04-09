951ad184574cd0fa28d626c0c37431ef7646d545
Sarannya Salim <sarannyasalim@gmail.com>
2019-04-09 23:43:43 +0200

    Add new items functionality complete

--- ToDoer/TodoerTableViewController.swift
+++ ToDoer/TodoerTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoerTableViewController: UITableViewController {
 
-    let itemsArray = ["Buy Milk", "Apply to Bosch", "Clean Drawers"]
+    var itemsArray = ["Buy Milk", "Apply to Bosch", "Clean Drawers"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +26,9 @@ class TodoerTableViewController: UITableViewController {
     // MARK: - Table view data source delegate methods
 
 
-    //--------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    //--------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
     {
         // #warning Incomplete implementation, return the number of rows
         return itemsArray.count
@@ -36,9 +36,9 @@ class TodoerTableViewController: UITableViewController {
     }
 
     
-    //--------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    //--------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------
     {
 
         let toDoItem = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath) as UITableViewCell
@@ -50,9 +50,9 @@ class TodoerTableViewController: UITableViewController {
     
     // MARK: - Table view delegate methods
     
-    //--------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    //--------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------
     {
         if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
         {
@@ -66,49 +66,29 @@ class TodoerTableViewController: UITableViewController {
         print(itemsArray[indexPath.row])
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+   // MARK: - Add New Items
+    
+    //-----------------------------------------------------------------------------------------------------
+    @IBAction func addButtinPressed(_ sender: UIBarButtonItem)
+    //-----------------------------------------------------------------------------------------------------
+    {
+        var itemTextField = UITextField()
+        let alert = UIAlertController(title: "Add new todoer item", message: "", preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("success!")
+            
+            if itemTextField.text != "" {
+                self.itemsArray.append(itemTextField.text!)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addTextField(configurationHandler: { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            itemTextField = alertTextField
+        })
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
